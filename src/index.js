1d21dcac3b3fd7b571571111fa4d9423661d743d
f1eeman <mr.f1eeman@gmail.com>
2020-05-29 01:49:07 +0600

    добавляет опцию выбора формата вывода

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import program from 'commander';
+
+export default () => {
+  program.version('0.0.1');
+  program.usage('[options] <filepath1> </filepath2>');
+  program.description('Compares two configuration files and shows a difference.');
+  program.option('-f, --format [type]', 'output format');
+  program.parse(process.argv);
+};
