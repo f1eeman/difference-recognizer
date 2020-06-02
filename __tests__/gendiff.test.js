e8354f5937248eab8c9c8c50f2f450a57b912400
f1eeman <mr.f1eeman@gmail.com>
2020-06-03 01:12:35 +0600

    Добавляет использование функции dirname()

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,20 @@
+import { dirname } from 'path';
 import genDiff from '../src/gendiff.js';
 
+let dirname1;
+let dirname2;
+let fileName1;
+let fileName2;
+
+beforeEach(() => {
+  dirname1 = dirname('__fixtures__/before.json');
+  dirname2 = dirname('__fixtures__/after.json');
+  fileName1 = 'before.json';
+  fileName2 = 'after.json';
+});
+
 test('gendiff', () => {
-  expect(genDiff('../frontend-project-lvl2/__fixtures__/before.json', '../frontend-project-lvl2/__fixtures__/after.json')).toMatch(`{
+  expect(genDiff(`${dirname1}/${fileName1}`, `${dirname2}/${fileName2}`)).toMatch(`{
     name: Mary
   + gender: female
   - gender: male
