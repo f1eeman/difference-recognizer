9f367211a19bbe563d57e7e2a3b2ea7fa98c99f2
f1eeman <mr.f1eeman@gmail.com>
2020-06-09 00:22:08 +0600

    Добавляет еще форматтер вывода - plain

---
+++ src/formatters/plain.js
@@ -0,0 +1,41 @@
+import _ from 'lodash';
+
+export default (diff, dataBefore, dataAfter) => {
+  const getStr = (obj, data1, data2, parent = '') => {
+    const keys = Object.keys(obj);
+    const entries = Object.entries(obj);
+
+    const result = entries.reduce((acc, [key, innerValue], currentIndex) => {
+      if (Array.isArray(innerValue)) {
+        const [objOfInnerValue] = innerValue;
+        const newParent = `${parent}${key}.`;
+        return [...acc, getStr(objOfInnerValue, data1[key], data2[key], newParent)];
+      }
+
+      if (innerValue.type === 'modified') {
+        if (_.isPlainObject(data1[keys[currentIndex]])) {
+          return [...acc, `Property '${parent}${keys[currentIndex]}' was changed from [complex value] to '${data2[keys[currentIndex]]}'`];
+        }
+        if (_.isPlainObject(data2[keys[currentIndex]])) {
+          return [...acc, `Property '${parent}${keys[currentIndex]}' was changed from '${data1[keys[currentIndex]]}' to [complex value]`];
+        }
+        return [...acc, `Property '${parent}${keys[currentIndex]}' was changed from '${data1[keys[currentIndex]]}' to '${data2[keys[currentIndex]]}'`];
+      }
+
+      if (innerValue.type === 'added') {
+        if (_.isPlainObject(data2[keys[currentIndex]])) {
+          return [...acc, `Property '${parent}${keys[currentIndex]}' was added with value: [complex value]`];
+        }
+        return [...acc, `Property '${parent}${keys[currentIndex]}' was added with value: ${data2[keys[currentIndex]]}`];
+      }
+
+      if (innerValue.type === 'deleted') {
+        return [...acc, `Property '${parent}${keys[currentIndex]}' was deleted`];
+      }
+      return acc;
+    }, []);
+
+    return result;
+  };
+  return _.flattenDeep(getStr(diff, dataBefore, dataAfter)).join('\n');
+};
--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import program from 'commander';
 import {
   genDiff, getExtName, getFilePath, getData, getParsedData, getDiff,
 } from './gendiff.js';
+import getDiffPlainType from './formatters/plain.js';
 
 export default () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => {
-      const extName1 = getExtName(filepath1);
-      const extName2 = getExtName(filepath2);
-      const path1 = getFilePath(filepath1);
-      const path2 = getFilePath(filepath2);
-      const data1 = getData(path1);
-      const data2 = getData(path2);
-      const parsedData1 = getParsedData(data1, extName1);
-      const parsedData2 = getParsedData(data2, extName2);
-      const processedData = genDiff(parsedData1, parsedData2);
-      console.log(getDiff(processedData, parsedData1, parsedData2));
-    });
+    .option('-f, --format [type]', 'output format', 'stylish')
+    .arguments('<filepath1> <filepath2>');
 
+  program.action((filepath1, filepath2) => {
+    const extName1 = getExtName(filepath1);
+    const extName2 = getExtName(filepath2);
+    const path1 = getFilePath(filepath1);
+    const path2 = getFilePath(filepath2);
+    const data1 = getData(path1);
+    const data2 = getData(path2);
+    const parsedData1 = getParsedData(data1, extName1);
+    const parsedData2 = getParsedData(data2, extName2);
+    const processedData = genDiff(parsedData1, parsedData2);
+    if (program.format === 'stylish') {
+      console.log(getDiff(processedData, parsedData1, parsedData2));
+    } else {
+      console.log(getDiffPlainType(processedData, parsedData1, parsedData2));
+    }
+  });
   program.parse(process.argv);
 };
