1cb845f8140308fc0016390db5c12b9836eb45df
f1eeman <mr.f1eeman@gmail.com>
2020-06-16 18:29:30 +0600

    refactoring ver.4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,20 +8,15 @@ let pathToIniFile1;
 let pathToIniFile2;
 let pathToYamlFile1;
 let pathToYamlFile2;
-let pathToResultStylishFormatterNestedStructure;
-let pathToResultPlainFormatterNestedStructure;
-let pathToResultJsonFormatterNestedStructure;
-let pathToResultStylishFormatterFlatStructure;
-let pathToResultPlainFormatterFlatStructure;
-let pathToResultJsonFormatterFlatStructureIni;
-let pathToResultJsonFormatterFlatStructureYaml;
-let resultStylishFormatterNestedStructure;
-let resultPlainFormatterNestedStructure;
-let resultJsonFormatterNestedStructure;
-let resultStylishFormatterFlatStructure;
-let resultPlainFormatterFlatStructure;
-let resultJsonFormatterFlatStructureIni;
-let resultJsonFormatterFlatStructureYaml;
+let pathToResultStylishFormatter;
+let pathToResultPlainFormatter;
+let pathToResultJsonFormatter;
+let pathToResultJsonFormatterIni;
+let resultStylishFormatter;
+let resultPlainFormatter;
+let resultJsonFormatter;
+let resultJsonFormatterIni;
+
 beforeAll(() => {
   const getFilePath = (fileName) => path.join(`${process.cwd()}/__fixtures__/${fileName}`);
   pathToJsonFile1 = getFilePath('before.json');
@@ -30,54 +25,48 @@ beforeAll(() => {
   pathToIniFile2 = getFilePath('after.ini');
   pathToYamlFile1 = getFilePath('before.yaml');
   pathToYamlFile2 = getFilePath('after.yaml');
-  pathToResultStylishFormatterNestedStructure = getFilePath('result-stylish-nested-structure.txt');
-  pathToResultPlainFormatterNestedStructure = getFilePath('result-plain-nested-structure.txt');
-  pathToResultJsonFormatterNestedStructure = getFilePath('result-json-nested-structure.json');
-  pathToResultStylishFormatterFlatStructure = getFilePath('result-stylish-flat-structure.txt');
-  pathToResultPlainFormatterFlatStructure = getFilePath('result-plain-flat-structure.txt');
-  pathToResultJsonFormatterFlatStructureIni = getFilePath('result-json-flat-structure-ini.json');
-  pathToResultJsonFormatterFlatStructureYaml = getFilePath('result-json-flat-structure-yaml.json');
-  resultStylishFormatterNestedStructure = fs.readFileSync(pathToResultStylishFormatterNestedStructure, 'utf8');
-  resultPlainFormatterNestedStructure = fs.readFileSync(pathToResultPlainFormatterNestedStructure, 'utf8');
-  resultJsonFormatterNestedStructure = fs.readFileSync(pathToResultJsonFormatterNestedStructure, 'utf8');
-  resultStylishFormatterFlatStructure = fs.readFileSync(pathToResultStylishFormatterFlatStructure, 'utf8');
-  resultPlainFormatterFlatStructure = fs.readFileSync(pathToResultPlainFormatterFlatStructure, 'utf8');
-  resultJsonFormatterFlatStructureIni = fs.readFileSync(pathToResultJsonFormatterFlatStructureIni, 'utf8');
-  resultJsonFormatterFlatStructureYaml = fs.readFileSync(pathToResultJsonFormatterFlatStructureYaml, 'utf8');
+  pathToResultStylishFormatter = getFilePath('result-stylish-formatter.txt');
+  pathToResultPlainFormatter = getFilePath('result-plain-formatter.txt');
+  pathToResultJsonFormatter = getFilePath('result-json-formatter.json');
+  pathToResultJsonFormatterIni = getFilePath('result-json-formatter-ini.json');
+  resultStylishFormatter = fs.readFileSync(pathToResultStylishFormatter, 'utf8');
+  resultPlainFormatter = fs.readFileSync(pathToResultPlainFormatter, 'utf8');
+  resultJsonFormatter = fs.readFileSync(pathToResultJsonFormatter, 'utf8');
+  resultJsonFormatterIni = fs.readFileSync(pathToResultJsonFormatterIni, 'utf8');
 });
 
-test('genDiffStylishTypeNestedStructure(JSON)', () => {
-  expect(genDiff(pathToJsonFile1, pathToJsonFile2)).toBe(resultStylishFormatterNestedStructure);
+test('genDiffStylishType(JSON)', () => {
+  expect(genDiff(pathToJsonFile1, pathToJsonFile2)).toBe(resultStylishFormatter);
 });
 
-test('getDiffPlainTypeNestedStructure(JSON)', () => {
-  expect(genDiff(pathToJsonFile1, pathToJsonFile2, 'plain')).toBe(resultPlainFormatterNestedStructure);
+test('getDiffPlainType(JSON)', () => {
+  expect(genDiff(pathToJsonFile1, pathToJsonFile2, 'plain')).toBe(resultPlainFormatter);
 });
 
-test('genDiffJsonTypeNestedStructure(JSON)', () => {
-  expect(genDiff(pathToJsonFile1, pathToJsonFile2, 'json')).toBe(resultJsonFormatterNestedStructure);
+test('genDiffJsonType(JSON)', () => {
+  expect(genDiff(pathToJsonFile1, pathToJsonFile2, 'json')).toBe(resultJsonFormatter);
 });
 
-test('genDiffStylishTypeFlatStructure(ini)', () => {
-  expect(genDiff(pathToIniFile1, pathToIniFile2)).toBe(resultStylishFormatterFlatStructure);
+test('genDiffStylishType(ini)', () => {
+  expect(genDiff(pathToIniFile1, pathToIniFile2)).toBe(resultStylishFormatter);
 });
 
-test('getDiffPlainTypeFlatStructure(ini)', () => {
-  expect(genDiff(pathToIniFile1, pathToIniFile2, 'plain')).toBe(resultPlainFormatterFlatStructure);
+test('getDiffPlainType(ini)', () => {
+  expect(genDiff(pathToIniFile1, pathToIniFile2, 'plain')).toBe(resultPlainFormatter);
 });
 
-test('genDiffJsonTypeFlatStructure(ini)', () => {
-  expect(genDiff(pathToIniFile1, pathToIniFile2, 'json')).toBe(resultJsonFormatterFlatStructureIni);
+test('genDiffJsonType(ini)', () => {
+  expect(genDiff(pathToIniFile1, pathToIniFile2, 'json')).toBe(resultJsonFormatterIni);
 });
 
-test('genDiffStylishTypeFlatStructure(yaml)', () => {
-  expect(genDiff(pathToYamlFile1, pathToYamlFile2)).toBe(resultStylishFormatterFlatStructure);
+test('genDiffStylishType(yaml)', () => {
+  expect(genDiff(pathToYamlFile1, pathToYamlFile2)).toBe(resultStylishFormatter);
 });
 
-test('getDiffPlainTypeFlatStructure(yaml)', () => {
-  expect(genDiff(pathToYamlFile1, pathToYamlFile2, 'plain')).toBe(resultPlainFormatterFlatStructure);
+test('getDiffPlainType(yaml)', () => {
+  expect(genDiff(pathToYamlFile1, pathToYamlFile2, 'plain')).toBe(resultPlainFormatter);
 });
 
-test('genDiffJsonTypeFlatStructure(yaml)', () => {
-  expect(genDiff(pathToYamlFile1, pathToYamlFile2, 'json')).toBe(resultJsonFormatterFlatStructureYaml);
+test('genDiffJsonType(yaml)', () => {
+  expect(genDiff(pathToYamlFile1, pathToYamlFile2, 'json')).toBe(resultJsonFormatter);
 });
