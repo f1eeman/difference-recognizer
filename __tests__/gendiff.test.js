879e4a5dc545f42516e3c2230868297bb24db0ab
f1eeman <mr.f1eeman@gmail.com>
2020-06-18 00:15:10 +0600

    refactoring tests ver.4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,30 +5,19 @@ import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const getFilePath = (fileName) => path.join(`${__dirname}/../__fixtures__/${fileName}`);
 const fileName1 = 'before';
 const fileName2 = 'after';
-let pathToResultStylishFormatter;
-let pathToResultPlainFormatter;
-let pathToResultJsonFormatter;
+const testArguments = ['json', 'ini', 'yaml'];
 let resultStylishFormatter;
 let resultPlainFormatter;
 let resultJsonFormatter;
 
-const testArguments = [
-  ['json'],
-  ['ini'],
-  ['yaml'],
-];
+const getFilePath = (fileName) => path.join(`${__dirname}`, '/../__fixtures__/', `${fileName}`);
 
 beforeAll(() => {
-  pathToResultStylishFormatter = getFilePath('result-stylish-formatter.txt');
-  pathToResultPlainFormatter = getFilePath('result-plain-formatter.txt');
-  pathToResultJsonFormatter = getFilePath('result-json-formatter.json');
-  resultStylishFormatter = fs.readFileSync(pathToResultStylishFormatter, 'utf8');
-  resultPlainFormatter = fs.readFileSync(pathToResultPlainFormatter, 'utf8');
-  resultJsonFormatter = fs.readFileSync(pathToResultJsonFormatter, 'utf8');
+  resultStylishFormatter = fs.readFileSync(getFilePath('result-stylish-formatter.txt'), 'utf8');
+  resultPlainFormatter = fs.readFileSync(getFilePath('result-plain-formatter.txt'), 'utf8');
+  resultJsonFormatter = fs.readFileSync(getFilePath('result-json-formatter.json'), 'utf8');
 });
 
 test.each(testArguments)('gendiff %s', (fileExtName) => {
