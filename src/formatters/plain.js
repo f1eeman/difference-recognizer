750e569a11d11c06cde4159731a5fd428780dd3c
f1eeman <mr.f1eeman@gmail.com>
2020-06-23 12:09:06 +0600

    refactoring ver.7

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,38 +4,41 @@ export default (data) => {
   const getStr = (obj, parent = '') => {
     const keys = Object.keys(obj);
     const entries = Object.entries(obj);
-    const result = entries.reduce((acc, [key, innerValue], currentIndex) => {
+
+    const result = entries.flatMap(([key, innerValue], currentIndex) => {
       if (Array.isArray(innerValue)) {
         const [objOfInnerValue] = innerValue;
         const newParent = `${parent}${key}.`;
-        return [...acc, getStr(objOfInnerValue, newParent)];
+        return getStr(objOfInnerValue, newParent);
       }
       if (innerValue.type === 'modified') {
         const [oldKey, newKey] = Object.keys(obj[keys[currentIndex]]);
         const oldValue = obj[keys[currentIndex]][oldKey];
         const newValue = obj[keys[currentIndex]][newKey];
         if (_.isPlainObject(oldValue)) {
-          return [...acc, `Property '${parent}${keys[currentIndex]}' was changed from [complex value] to '${newValue}'`];
+          return `Property '${parent}${keys[currentIndex]}' was changed from [complex value] to '${newValue}'`;
         }
         if (_.isPlainObject(newValue)) {
-          return [...acc, `Property '${parent}${keys[currentIndex]}' was changed from '${oldValue}' to [complex value]`];
+          return `Property '${parent}${keys[currentIndex]}' was changed from '${oldValue}' to [complex value]`;
         }
-        return [...acc, `Property '${parent}${keys[currentIndex]}' was changed from '${oldValue}' to '${newValue}'`];
+        return `Property '${parent}${keys[currentIndex]}' was changed from '${oldValue}' to '${newValue}'`;
       }
+
       if (innerValue.type === 'added') {
         const [keyOfNewValue] = Object.keys(obj[keys[currentIndex]]);
         const newValue = obj[keys[currentIndex]][keyOfNewValue];
         if (_.isPlainObject(newValue)) {
-          return [...acc, `Property '${parent}${keys[currentIndex]}' was added with value: [complex value]`];
+          return `Property '${parent}${keys[currentIndex]}' was added with value: [complex value]`;
         }
-        return [...acc, `Property '${parent}${keys[currentIndex]}' was added with value: ${newValue}`];
+        return `Property '${parent}${keys[currentIndex]}' was added with value: ${newValue}`;
       }
+
       if (innerValue.type === 'deleted') {
-        return [...acc, `Property '${parent}${keys[currentIndex]}' was deleted`];
+        return `Property '${parent}${keys[currentIndex]}' was deleted`;
       }
-      return acc;
-    }, []);
+      return '';
+    });
     return result;
   };
-  return _.flattenDeep(getStr(data)).join('\n');
+  return getStr(data).filter((el) => el !== '').join('\n');
 };
