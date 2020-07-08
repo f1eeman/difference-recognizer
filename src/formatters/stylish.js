5cbf28301573268f48139522c30eb0ea254a9015
f1eeman <mr.f1eeman@gmail.com>
2020-07-08 12:18:59 +0600

    рефакторит функцию по возврату отступов на основе одного парамета - глубины

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,47 +6,45 @@ const getSubstr = (beginIndent, endIndent, value) => {
   return `{\n${beginIndent}${key}: ${property}\n${endIndent}}`;
 };
 
-const getIndent = (isUnchangedValues, isNestedValues, isParentKey, isCloseBracket, depth) => {
-  const stepForSpacesCount = 4;
-  const stepForSpacesForChangedValuesCount = 2;
-  if (isUnchangedValues || isParentKey || isCloseBracket) {
-    return ' '.repeat(depth * stepForSpacesCount);
-  }
-  if (isNestedValues) {
-    return ' '.repeat((depth * stepForSpacesCount) + stepForSpacesCount);
-  }
-  return ' '.repeat((depth * stepForSpacesCount) - stepForSpacesForChangedValuesCount);
+const getIndent = (depth) => {
+  const stepsForIndentCount = 4;
+  return ' '.repeat(depth * stepsForIndentCount);
 };
 
-const getStrInStylishType = (tree, depth = 1) => {
-  const result = tree.map((node) => {
-    const indentOut = getIndent(false, false, false, false, depth);
-    const indentInner = getIndent(false, true, false, false, depth);
-    const indentOutForUnchangedValues = getIndent(true, false, false, false, depth);
-    const indentBeforeCloseBracket = getIndent(false, false, false, true, depth);
-    const indentForParentKey = getIndent(false, false, true, false, depth);
-    const {
-      key, type, value, value1, value2, children,
-    } = node;
-    const substrForValue = getSubstr(indentInner, indentBeforeCloseBracket, value);
-    const substForValue1 = getSubstr(indentInner, indentBeforeCloseBracket, value1);
-    const substrForValue2 = getSubstr(indentInner, indentBeforeCloseBracket, value2);
-    switch (type) {
-      case 'parent':
-        return `${indentForParentKey}${key}: {\n${getStrInStylishType(children, depth + 1)}\n${indentBeforeCloseBracket}}`;
-      case 'modified':
-        return `${indentOut}- ${key}: ${substForValue1}\n${indentOut}+ ${key}: ${substrForValue2}`;
-      case 'added':
-        return `${indentOut}+ ${key}: ${substrForValue}`;
-      case 'deleted':
-        return `${indentOut}- ${key}: ${substrForValue}`;
-      case 'unchanged':
-        return `${indentOutForUnchangedValues}${key}: ${substrForValue}`;
-      default:
-        throw new Error(`Unknown type: ${type}`);
-    }
-  });
-  return result.join('\n');
+const getStrInStylishType = (tree) => {
+  const iter = (data, depth = 1) => {
+    const result = data.map((node) => {
+      const stepsForInnerIndentCount = 4;
+      const stepsForIndentForChangedValuesCount = 2;
+      const indentOutForChangedValues = ' '.repeat((depth * stepsForInnerIndentCount) - stepsForIndentForChangedValuesCount);
+      const indentInner = ' '.repeat((depth * stepsForInnerIndentCount) + stepsForInnerIndentCount);
+      const indentOutForUnchangedValues = getIndent(depth);
+      const indentBeforeCloseBracket = getIndent(depth);
+      const indentForParentKey = getIndent(depth);
+      const {
+        key, type, value, value1, value2, children,
+      } = node;
+      const substrForValue = getSubstr(indentInner, indentBeforeCloseBracket, value);
+      const substForValue1 = getSubstr(indentInner, indentBeforeCloseBracket, value1);
+      const substrForValue2 = getSubstr(indentInner, indentBeforeCloseBracket, value2);
+      switch (type) {
+        case 'parent':
+          return `${indentForParentKey}${key}: {\n${iter(children, depth + 1)}\n${indentBeforeCloseBracket}}`;
+        case 'modified':
+          return `${indentOutForChangedValues}- ${key}: ${substForValue1}\n${indentOutForChangedValues}+ ${key}: ${substrForValue2}`;
+        case 'added':
+          return `${indentOutForChangedValues}+ ${key}: ${substrForValue}`;
+        case 'deleted':
+          return `${indentOutForChangedValues}- ${key}: ${substrForValue}`;
+        case 'unchanged':
+          return `${indentOutForUnchangedValues}${key}: ${substrForValue}`;
+        default:
+          throw new Error(`Unknown type: ${type}`);
+      }
+    });
+    return result.join('\n');
+  };
+  return iter(tree);
 };
 
 export default (data) => `{\n${getStrInStylishType(data)}\n}`;
