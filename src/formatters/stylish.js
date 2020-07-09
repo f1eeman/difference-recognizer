a692614af30b559d28047533567d8ad02b5fd357
f1eeman <mr.f1eeman@gmail.com>
2020-07-09 13:32:32 +0600

    выделение в отдельную функцию получение строки в stylish.js, refactoring функции по получению строки свойства в зав-ти от его типа

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,44 +1,44 @@
 import _ from 'lodash';
 
-const additionalIndentForChangedValues = '  ';
-const additionalOutIndent = '    ';
-const additionalInnerIndent = '        ';
+const additionalIndentForChangedValues = ' '.repeat(2);
+const additionalOutIndent = ' '.repeat(4);
+const additionalInnerIndent = ' '.repeat(8);
 
 const getIndent = (depth) => '    '.repeat(depth);
 
-const getSubstr = (beginIndent, endIndent, value) => {
+const getSubstr = (value, depth) => {
   if (!_.isPlainObject(value)) return value;
   const [key, property] = Object.entries(value).flat();
-  return `{\n${beginIndent}${key}: ${property}\n${endIndent}}`;
+  const indent = getIndent(depth);
+  return `{\n${indent}${additionalInnerIndent}${key}: ${property}\n${indent}${additionalOutIndent}}`;
+};
+
+const getStrings = (node, depth, callback) => {
+  const indent = getIndent(depth);
+  const {
+    key, type, value, value1, value2, children,
+  } = node;
+  const substrForValue = getSubstr(value, depth);
+  const substForValue1 = getSubstr(value1, depth);
+  const substrForValue2 = getSubstr(value2, depth);
+  switch (type) {
+    case 'parent':
+      return [`${indent}${additionalOutIndent}${key}: {\n${callback(children, depth + 1)}`, `${indent}${additionalOutIndent}}`];
+    case 'modified':
+      return [`${indent}${additionalIndentForChangedValues}- ${key}: ${substForValue1}`, `${indent}${additionalIndentForChangedValues}+ ${key}: ${substrForValue2}`];
+    case 'added':
+      return [`${indent}${additionalIndentForChangedValues}+ ${key}: ${substrForValue}`];
+    case 'deleted':
+      return [`${indent}${additionalIndentForChangedValues}- ${key}: ${substrForValue}`];
+    case 'unchanged':
+      return [`${indent}${additionalOutIndent}${key}: ${substrForValue}`];
+    default:
+      throw new Error(`Unknown type: ${type}`);
+  }
 };
 
 const getStrInStylishType = (tree) => {
-  const iter = (data, depth = 0) => {
-    const result = data.map((node) => {
-      const indent = getIndent(depth);
-      const {
-        key, type, value, value1, value2, children,
-      } = node;
-      const substrForValue = getSubstr(`${indent}${additionalInnerIndent}`, `${indent}${additionalOutIndent}`, value);
-      const substForValue1 = getSubstr(`${indent}${additionalInnerIndent}`, `${indent}${additionalOutIndent}`, value1);
-      const substrForValue2 = getSubstr(`${indent}${additionalInnerIndent}`, `${indent}${additionalOutIndent}`, value2);
-      switch (type) {
-        case 'parent':
-          return `${indent}${additionalOutIndent}${key}: {\n${iter(children, depth + 1)}\n${indent}${additionalOutIndent}}`;
-        case 'modified':
-          return `${indent}${additionalIndentForChangedValues}- ${key}: ${substForValue1}\n${indent}${additionalIndentForChangedValues}+ ${key}: ${substrForValue2}`;
-        case 'added':
-          return `${indent}${additionalIndentForChangedValues}+ ${key}: ${substrForValue}`;
-        case 'deleted':
-          return `${indent}${additionalIndentForChangedValues}- ${key}: ${substrForValue}`;
-        case 'unchanged':
-          return `${indent}${additionalOutIndent}${key}: ${substrForValue}`;
-        default:
-          throw new Error(`Unknown type: ${type}`);
-      }
-    });
-    return result.join('\n');
-  };
+  const iter = (children, depth = 0) => children.flatMap((child) => getStrings(child, depth, iter)).join('\n');
   return iter(tree);
 };
 
